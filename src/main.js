74ce392cd9d37f9b5be835860e832678195e6e0a
dwc <17683801735@163.com>
2020-08-26 21:50:13 +0800

    配置了项目中的vant

--- src/main.js
+++ src/main.js
@@ -1,6 +1,15 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+import './styles/base.less'
+import './styles/iconfont.css'
+// 第三种方式
+import Vant from 'vant'
+import 'vant/lib/index.css'
+Vue.use(Vant)
+// 第一种方式
+// import { Button } from 'vant'
+// Vue.use(Button)
 
 Vue.config.productionTip = false
 
--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,5 +1,14 @@
 <template>
-  <div>登录组件</div>
+  <div>
+    登录组件
+    <span class="iconfont iconweixin"></span>
+    <span class="iconfont icondianzan"></span>
+    <van-button type="primary">主要按钮</van-button>
+    <van-button type="info">信息按钮</van-button>
+    <van-button type="default">默认按钮</van-button>
+    <van-button type="warning">警告按钮</van-button>
+    <van-button type="danger">危险按钮</van-button>
+  </div>
 </template>
 
 <script>
